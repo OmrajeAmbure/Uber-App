7ffe5d8b30c5aa6e4b572e09bb9482b7d87069e7
OmrajeAmbure <omrajeambure27@gmail.com>
2024-12-26 22:12:42 +0530

    first commit

---
+++ Backend/server.js
@@ -0,0 +1,10 @@
+const port = process.env.PORT || 3000;
+const http = require('http');
+const app = require('./index');
+
+
+const server = http.createServer(app);
+
+app.listen(port, (req, res) => {
+    console.log(`Server is running on port ${port}`);
+});
